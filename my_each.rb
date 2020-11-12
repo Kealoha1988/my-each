65766ee16ab8ec52f3c743b1513ac7b3526bb1b0
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-12 14:27:59 -0500

    commit tree cc914c0e3495db0ce0b8fc461aa1b5c0fbaf9f2b

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array)
+  i = 0
+  while i < array.length
+     yield(array[i])
+     i = i +1
+    end
+    array
+  end
+
+dude = ["Hen", "Ben", "Lin", "Ken"]
+
+my_each(dude) {|i| "Hi #{i}"}
